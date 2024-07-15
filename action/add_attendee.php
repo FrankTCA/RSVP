be3c370354dfd9118d438ca0607d01e07e4d3161
Frank <frank@steamedveggie.com>
2024-07-15 15:11:08 -0400

    Add and get attendees.

---
+++ action/add_attendee.php
@@ -0,0 +1,89 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+if (!(isset($_POST["event_id"]) && isset($_POST["name"]))) {
+    http_response_code(400);
+    die("noinfo");
+}
+
+$event_id = $_POST["event_id"];
+$attendee_name = $_POST["name"];
+$attendee_email = $_POST["email"];
+
+validate_token("https://infotoast.org/rsvp/action/add_attendee.php");
+$uid = get_user_id();
+
+$conn = mysqli_connect(get_database_host(), get_database_user(), get_database_password(), get_database_db());
+if ($conn->connect_error) {
+    http_response_code(500);
+    die("dbconn");
+}
+
+$event_name = "";
+
+// Make sure event is owned by user
+$sql = $conn->prepare("SELECT * FROM events WHERE id = ? AND user_id = ?;");
+$sql->bind_param("ii", $event_id, $uid);
+$sql->execute();
+$passed_check = false;
+if ($result = $sql->get_result()) {
+    while ($row = $result->fetch_assoc()) {
+        $passed_check = $row["id"] == $event_id;
+        $event_name = $row["name"];
+    }
+}
+
+if (!$passed_check) {
+    http_response_code(403);
+    die("unowned");
+}
+
+$token_str = random_bytes(16);
+
+$sql2 = $conn->prepare("INSERT INTO attendees (event_id, name, email, token) VALUES (?, ?, ?, LEFT(SHA2(?, 256), 10));");
+$sql2->bind_param("isss", $event_id, $attendee_name, $attendee_email, $token_str);
+$sql2->execute();
+
+$attendee_data_sql = $conn->prepare("SELECT id,token FROM attendees WHERE id = LAST_INSERT_ID() AND event_id = ?;");
+$attendee_data_sql->bind_param("i", $event_id);
+$attendee_data_sql->execute();
+
+$token = null;
+$attendee_id = null;
+
+if ($result = $attendee_data_sql->get_result()) {
+    while ($row = $result->fetch_assoc()) {
+        $attendee_id = $row["id"];
+        $token = $row["token"];
+    }
+}
+
+$rsvp_link = "https://infotoast.org/rsvp/rsvp.php?t=$token";
+
+if ($attendee_email != null) {
+    $mail = new PHPMailer(true);
+    $mail->SMTPDebug = 2;
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = 'tls';
+    $mail->Port = 587;
+    $mail->Username = "no-reply@infotoast.org";
+    $mail->Password = get_noreply_password();
+    $organizer_email = get_user_email();
+    $mail->setFrom("no-reply@infotoast.org", "$organizer_email via Info Toast RSVP");
+    $mail->addAddress($attendee_email);
+    $mail->addReplyTo($organizer_email);
+    $mail->isHTML(true);
+
+    $mail->Subject = "RSVP to $event_name";
+    $mail->Body = "<h1>You have been invited to $event_name</h1><br><br><h3>Please RSVP to <a href='mailto:$organizer_email'>$organizer_email</a></h3><br><br><p><strong>You can RSVP with <a href='$rsvp_link'>our online tool here.</a></strong></p><p>If the above link does not work, please copy and paste $rsvp_link in your browser.</p>";
+    $mail->AltBody = "You have been invited to $event_name. Please RSVP to $organizer_email with our online tool here: $rsvp_link";
+    $mail->send();
+}
+
+echo "success,$attendee_id,$token";
---
+++ action/get_attendees_info.php
@@ -0,0 +1,95 @@
+<?php
+
+if (!isset($_GET["event_id"])) {
+    http_response_code(400);
+    die("noinfo");
+}
+$event_id = $_GET["event_id"];
+
+validate_token("https://infotoast.org/rsvp/action/get_attendees_info.php");
+$uid = get_user_id();
+
+$conn = mysqli_connect(get_database_host(), get_database_user(), get_database_password(), get_database_db());
+if ($conn->connect_error) {
+    http_response_code(500);
+    die("dbconn");
+}
+
+// Validate whether user owns event
+$sql = $conn->prepare("SELECT * FROM events WHERE id = ? AND user_id = ?;");
+$sql->bind_param("ii", $event_id, $uid);
+$sql->execute();
+$passed_check = false;
+if ($result = $sql->get_result()) {
+    while ($row = $result->fetch_assoc()) {
+        $passed_check = $row["id"] == $event_id;
+    }
+}
+
+if (!$passed_check) {
+    http_response_code(403);
+    die("unowned");
+}
+
+$sql2 = $conn->prepare("SELECT * FROM attendees WHERE event_id = ?;");
+$sql2->bind_param("i", $event_id);
+$sql2->execute();
+
+$count_responded = 0;
+$count_not_responded = 0;
+$count_yes = 0;
+$count_no = 0;
+$count_maybe = 0;
+$count_unanswered = 0;
+
+echo "{\"responses\": [";
+$count = 0;
+
+if ($result2 = $sql2->get_result()) {
+    while ($row = $result2->fetch_assoc()) {
+        if ($count != 0) {
+            echo ", ";
+        }
+
+        echo "{\"name\": \"" . $row["name"] . "\", ";
+        echo "\"date_added\": \"" . $row["date_added"] . "\", ";
+        echo "\"accessed\": " . $row["accessed"] . ", ";
+        if ($row["accessed"]) {
+            $count_responded++;
+            echo "\"time_accessed\": \"" . $row["time_accessed"] . "\", ";
+            echo "\"country_code\": \"" . $row["country_code"] . "\", ";
+        } else {
+            $count_not_responded++;
+        }
+        echo "\"response\": " . $row["response"] . ", ";
+        if ($row["response"] != 0) {
+            if ($row["response"] == 1) {
+                $count_yes++;
+            } else if ($row["response"] == 2) {
+                $count_no++;
+            } else if ($row["response"] == 3) {
+                $count_maybe++;
+            }
+            if ($row["response_note"] != null && $row["response_note"] != "") {
+                echo "\"response_note\": \"" . $row["response_note"] . "\"";
+            } else {
+                echo "\"response_note\": null";
+            }
+        } else {
+            $count_unanswered++;
+        }
+        echo "}";
+        $count++;
+    }
+}
+
+echo "], \"total\": " . $count . ", ";
+echo "\"responded\": " . $count_responded . ", ";
+echo "\"not_responded\": " . $count_not_responded . ", ";
+echo "\"yes\": " . $count_yes . ", ";
+echo "\"no\": " . $count_no . ", ";
+echo "\"maybe\": " . $count_maybe . ", ";
+echo "\"unanswered\": " . $count_unanswered;
+echo "}";
+
+
